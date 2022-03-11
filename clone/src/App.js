9c445beb76dafb2b473dfdcbeb2c787e35ce5c9a
Marcos <marcosmaiafidelis777@outlook.com>
2022-03-11 11:27:46 -0300

    posters

--- clone/src/App.js
+++ clone/src/App.js
@@ -20,7 +20,7 @@ export default () => {
         <div>
             <section>
                 {movieList.map((item, key) => (
-                     <MovieRow/>
+                     <MovieRow key={key} title={item.title} items={item.items}/>
                 ))}
             </section>
         </div>
--- clone/src/components/MovieRow.js
+++ clone/src/components/MovieRow.js
@@ -1,10 +1,23 @@
 import React from "react";
-import './MovieRow.css'
+import './MovieRow.css';
 
-export default () => {
+export default ({title, items}) => {
     return(
         <div>
-            Lista de Filmes
+            <h2>{title}</h2>
+            <div className="movieRow--listarea">
+            {items.results.length > 0 && items.results.map((item,key)=>(
+            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_ti}/>
+            ))}
+            </div>
         </div>
-    )
-}
+    );
+}   
+
+        
+
+                
+
+        
+    
+             
