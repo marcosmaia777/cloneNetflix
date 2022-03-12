3a9eb1a29181ea4b6692a105f736083d7fab3539
Marcos <marcosmaiafidelis777@outlook.com>
2022-03-12 20:54:42 -0300

    Detaque

--- clone/src/App.js
+++ clone/src/App.js
@@ -2,10 +2,13 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Tmdb from './Tmdb'; 
 import MovieRow from './components/MovieRow'; 
+import FaturedMovie from './components/FeaturedMovie';
+
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setfeatured] = useState
    
     useEffect(() => {
         const loadAll = async ()=> {
@@ -18,8 +21,9 @@ export default () => {
     },[]);
    
     return(
-        <div>
-            <section>
+        <div className='page'>
+
+            <section className='lists'>
                 {movieList.map((item, key) => (
                      <MovieRow key={key} title={item.title} items={item.items}/>
                 ))}
---
+++ clone/src/components/FeaturedMovie.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './FaturedMovie.css'
+
+export default () => {
+    return(
+        <div>
+            Destaques
+        </div>
+    )
+}
