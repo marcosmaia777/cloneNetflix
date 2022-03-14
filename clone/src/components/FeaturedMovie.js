249dc0551459bd410d2b1a5264930cf8d8f3f896
Marcos <marcosmaiafidelis777@outlook.com>
2022-03-14 17:13:54 -0300

    serie destaque

--- clone/src/components/FeaturedMovie.js
+++ clone/src/components/FeaturedMovie.js
@@ -14,8 +14,9 @@ export default ({item}) => {
                     <div className="featured--info">
                     <div className="featured--points">{item.vote_average} pontos</div>
                     <div className="featured--year">2099</div>
-                    <div className="featured--seasons">{item.number_of_seasons}</div>
+                    <div className="featured--seasons">{item.number_of_seasons !== 1 ? "s" : ""} temporadas</div>
                    </div>
+                   <div className="featured--description">{item.overview}</div>
             </div>
         </div>
         </section>
