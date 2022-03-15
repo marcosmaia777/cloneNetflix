c58068a80f148ad313eea16f9f086d0ff4687004
Marcos <marcosmaiafidelis777@outlook.com>
2022-03-15 20:32:48 -0300

    informações tela destaque

--- clone/src/components/FeaturedMovie.js
+++ clone/src/components/FeaturedMovie.js
@@ -2,8 +2,13 @@ import React from "react";
 import './FeaturedMovie.css'
 
 export default ({item}) => {
+    console.log(item);
 
     let fistDate = new Date(item._air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
 
     return(
         <section className="featured" style={{
@@ -15,14 +20,14 @@ export default ({item}) => {
                 <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                     <div className="featured--points">{item.vote_average} pontos</div>
-                    <div className="featured--year">2099</div>
+                    <div className="featured--year">{fistDate.getFullYear()}</div>
                     <div className="featured--seasons">{item.number_of_seasons !== 1 ? "s" : ""} temporadas</div>
                    </div>
                    <div className="featured--description">{item.overview}</div>
                    <div className="featured--buttons">
 
                    </div>
-                   <div className="featured--genres"><strong>Gêneros:</strong></div>
+                   <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
             </div>
         </div>
         </section>
