9076e58c3b8eafa5ad56f23525b578b10bfea9c7
Marcos <marcosmaiafidelis777@outlook.com>
2022-03-22 22:33:52 -0300

    header

--- clone/src/components/Header.js
+++ clone/src/components/Header.js
@@ -4,7 +4,16 @@ import './Header.css'
 export default () => {
     return(
        <header>
-           ...
+           <div className='header--logo'>
+               <a href="/">
+                   <img src='https://img.elo7.com.br/product/original/2F57C45/poster-impresso-netflix-logo-basquete.jpg' alt='netflix'/>
+               </a>
+           </div>
+           <div className='header--user'>
+               <a href='/'>
+                   <img src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png' alt='usuário'/>
+               </a>
+           </div>
        </header>
     );
 }
